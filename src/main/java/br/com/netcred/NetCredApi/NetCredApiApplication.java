687d7bb808f8b5d86324b5968a01c015c4e490eb
alankaiko <alamdfs@hotmail.com>
2024-11-20 08:49:03 -0300

    confiugracoes base

---
+++ src/main/java/br/com/netcred/NetCredApi/NetCredApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.netcred.NetCredApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCredApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetCredApiApplication.class, args);
+		System.out.println("APLICAÇÃO FUNCIONANDO ");
+	}
+
+}
